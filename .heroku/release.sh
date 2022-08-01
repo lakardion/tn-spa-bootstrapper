a8a3690fe0f7df7913711017ea5495a212f45969
Ed Romano <oudeis.metis@gmail.com>
2022-08-01 15:17:48 -0400

    See if we can trigger a test project

--- .heroku/release.sh
+++ .heroku/release.sh
@@ -11,11 +11,11 @@ pr=$HEROKU_PR_NUMBER
 curl -X POST \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer $token" \
-     https://api.github.com/repos/$repo/dispatches \
+     https://api.github.com/repos/thinknimble/my_project/dispatches \
      -d '{"event_type":"review-app-test",
        "client_payload":{
          "review_app_url": "https://$app.herokuapp.com",
-         "PR_NUM": "$pr"
+         "PR_NUM": "1"
        }
      }'
 
